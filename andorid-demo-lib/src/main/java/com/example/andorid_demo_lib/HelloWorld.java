38ffa071ebd17bcceb9192bb67329e22a525755c
AhmedMLasheen <85111481+AhmedMLasheen@users.noreply.github.com>
2023-11-19 12:59:38 +0200

    git commit

---
+++ andorid-demo-lib/src/main/java/com/example/andorid_demo_lib/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.andorid_demo_lib;
+
+
+import android.util.Log;
+
+public class HelloWorld {
+    public  static  void main(String [] args)
+    {
+
+    }
+    public  static void printlog(String tag , String message)
+    {
+        Log.d(tag , message) ;
+    }
+}
